b9ecdaeba7da52a280307832ac76eb83cb89c411
krosturbo <krosturbo@gmail.com>
2023-08-06 00:29:30 +0200

    Summary

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var form = document.getElementById('formId');
+function submitForm(event) {
+	//Preventing page refresh
+	const money = document.getElementById('money').value;
+	const share = document.getElementById('share').value;
+	const result = document.getElementById('result');
+	const result2 = document.getElementById('result2');
+	result.innerHTML = 'Potencjalny łup: ' + money * 0.88;
+	result2.innerHTML = 'Twój łup: ' + money * 0.88 * (share / 100);
+
+	event.preventDefault();
+}
+
+//Calling a function during form submission.
+form.addEventListener('submit', submitForm);
